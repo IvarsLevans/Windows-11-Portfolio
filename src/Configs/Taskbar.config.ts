04b91d544435fdedc853a2c3828e592df858fd13
ivarslevans <ivars.levans@gmail.com>
2023-02-19 19:53:22 +0200

    Start Menu Icons

--- src/Configs/Taskbar.config.ts
+++ src/Configs/Taskbar.config.ts
@@ -1,5 +1,11 @@
+import taskmanager from '../assets/icons/taskbar/taskmanager.png'
+
 export interface icon {
   name: string
   url: string
   action?: () => any
 }
+export const ICONS: icon[] = [{
+  name: "Task Manager",
+  url: taskmanager
+}]
